9e3573235e7b3d10aaa93be28d85ac715021f96d
Sergio Villasenor <42443963+thatotherkid86@users.noreply.github.com>
2022-09-16 21:06:13 +0300

    updated meta tags

--- components/meta/meta.js
+++ components/meta/meta.js
@@ -8,7 +8,14 @@ export default function Meta() {
             <meta name="description" content="NFTPort API starter kit using Next.js, Tailwind and Vercel."/>
             <meta name="viewport" content="width=device-width,maximum-scale=1"/>
             <link rel="icon" href="/favicon.ico" />
-            <meta property="og:url" content="http://www.imdb.com/title/tt0117500/"/>
+            <meta property="og:title" content="NFTPort | NFT Portfolio Starter Kit" />
+            <meta property="og:type" content="article" />
+            <meta property="og:url" content="https://bored-apes-nft-landing-page-xrdg.vercel.app/" />
+            <meta property="og:image" content="https://bored-apes-nft-landing-page-xrdg.vercel.app/og.jpg" />
+            <meta name="twitter:title" content="NFTPort | NFT Portfolio Starter Kit" />
+            <meta name="twitter:description" content="NFTPort API starter kit using Next.js, Tailwind and Vercel." />
+            <meta name="twitter:image" content="https://bored-apes-nft-landing-page-xrdg.vercel.app/og.jpg" />
+            <meta name="twitter:card" content="summary_large_image"></meta>
         </Head>
         </>
     )
